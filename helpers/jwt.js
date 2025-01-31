3a3f0527bfc88df83aab02abd963182eda7d9d25
krishnamounica <krishna.mernstack@gmail.com>
2025-01-31 15:50:09 +0530

    jwt skipped for raise request

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -13,6 +13,8 @@ function authJwt() {
             { url: /\/api\/v1\/products(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/categories(.*)/, methods: ['GET', 'OPTIONS'] },
             { url: /\/api\/v1\/orders(.*)/, methods: ['GET', 'OPTIONS', 'POST'] },
+            { url: /\/api\/v1\/users\/[^/]+$/, methods: ['PUT'] },
+
             `${api}/users/login`,
             `${api}/users/register`
             // { url: /(.*)/ },
