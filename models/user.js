a1a52120fc0db760888a47d11bc3555f33d2e42b
krishnamounica <krishna.mernstack@gmail.com>
2025-04-28 23:39:14 +0530

    password and phone makes optional

--- models/user.js
+++ models/user.js
@@ -1,5 +1,33 @@
 const mongoose = require('mongoose');
 
+const requestSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    phone: { type: String, required: true },
+    relation: { type: String, required: true },
+    occasion: { type: String, required: true },
+    date: { type: Date, required: true },
+    flatNumber: { type: String, required: true },
+    building: { type: String, required: true },
+    landmark: { type: String, required: true },
+    district: { type: String, required: true },
+    state: { type: String, required: true },
+    pincode: { type: String, required: true },
+    productId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Product" }, // Reference to Product Model
+    productName: { type: String, required: true },
+    productPrice: { type: Number, required: true }, // Ensure price is stored as a number
+    status: { type: String, enum: ["pending", "approved", "rejected"], default: "pending" },
+    id: { type: String, required: true, unique: true }, // Ensure ID is stored as a string
+    feedback: { type: [String], default: [] },
+    payment: { type: Boolean, default: false },
+    sharable: { type: Boolean, default: false },
+    userName: { type: String, required: true },
+    paymentlink: { type: String },
+    sharablelink: { type: String },
+    totalAmount: { type: Number, required: true },
+    remaingamount: { type: Number, default: 0 },
+    noofpayments: { type: Number, default: 0 }
+}, { _id: false }); // Prevents mongoose from adding an automatic `_id` inside requests
+
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -11,11 +39,11 @@ const userSchema = new mongoose.Schema({
     },
     passwordHash: {
         type: String,
-        required: true,
+        required: false,
     },
     phone: {
         type: String,
-        required: true,
+        required: false,
     },
     isAdmin: {
         type: Boolean,
