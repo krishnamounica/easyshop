edc6444365afb4871616c3268bd063ea8cfaaa14
krishnamounica <krishna.mernstack@gmail.com>
2025-04-30 14:26:41 +0530

    Update payment.js

--- models/payment.js
+++ models/payment.js
@@ -1,6 +1,4 @@
-
 const mongoose = require('mongoose');
-
 const paymentSchema = new mongoose.Schema({
   razorpay_payment_id: { type: String, required: true },
   razorpay_order_id: { type: String, required: true },
@@ -8,7 +6,9 @@ const paymentSchema = new mongoose.Schema({
   productId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Product" },
   userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   createdAt: { type: Date, default: Date.now },
-  amount:{type:String,required:true}
+  amount:{type:String,required:true},
+  payment_mode: { type: String },
+tax: { type: Number },
+amount: { type: Number }
 });
-
 module.exports = mongoose.model('Payment', paymentSchema);
