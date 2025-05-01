8282817df2a6e1b6b10ac19aa286778c6e397234
krishnamounica <krishna.mernstack@gmail.com>
2025-05-01 12:30:20 +0530

    Update gifts.js

--- routes/gifts.js
+++ routes/gifts.js
@@ -8,14 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// MongoDB Connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log("DB Connection Error:", err));
 
-// Gift Route - POST request to save gift details
 app.post('/', async (req, res) => {
     const {
         name, phone, relation, occasion, date, flatNumber, building, landmark, district, state, productId, productName, productPrice, status,
