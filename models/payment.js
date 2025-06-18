e2a11e09e8b92dd0fa743bfa570e283a5067b3a6
krishnamounica <krishna.mernstack@gmail.com>
2025-06-18 19:51:01 +0530

    made address as array

--- models/payment.js
+++ models/payment.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+
 const paymentSchema = new mongoose.Schema({
   razorpay_payment_id: { type: String, required: true },
   razorpay_order_id: { type: String, required: true },
@@ -6,17 +7,19 @@ const paymentSchema = new mongoose.Schema({
   productId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Product" },
   userId: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   createdAt: { type: Date, default: Date.now },
-  amount:{type:String,required:true},
+  amount: { type: Number, required: true },
+  tax: { type: Number },
   payment_mode: { type: String },
-tax: { type: Number },
-amount: { type: Number },
- address: {
-    fullName: { type: String, required: true },
-    street: { type: String, required: true },
-    city: { type: String, required: true },
-    state: { type: String },
-    postalCode: { type: String },
-    country: { type: String },
-  }
+  address: [
+    {
+      fullName: { type: String, required: true },
+      street: { type: String, required: true },
+      city: { type: String, required: true },
+      state: { type: String },
+      postalCode: { type: String },
+      country: { type: String },
+    }
+  ]
 });
+
 module.exports = mongoose.model('Payment', paymentSchema);
