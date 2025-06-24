648d57ac5549dd1eb778009324bbf4677bfc079b
krishnamounica <krishna.mernstack@gmail.com>
2025-06-24 20:39:07 +0530

    Create occasionMessages.js

---
+++ helpers/occasionMessages.js
@@ -0,0 +1,54 @@
+// utils/occasionMessages.js
+
+const messages = {
+  birthday: [
+    "Happy Birthday! May all your dreams come true!",
+    "Wishing you a day filled with love and laughter!",
+    "Cheers to another year of awesomeness!",
+    "Have a fantastic birthday!",
+    "Happy Birthday! Stay blessed always!",
+    "May your day be as amazing as you are!",
+    "Sending smiles and good vibes your way!",
+    "Enjoy your special day to the fullest!",
+    "You deserve all the cake and joy today!",
+    "Wishing you happiness today and always!"
+  ],
+  wedding: [
+    "Wishing you a lifetime of love and joy!",
+    "Congrats on your wedding day!",
+    "Two hearts, one love. Happy Wedding!",
+    "May your union be filled with blessings!",
+    "Here's to love, laughter, and happily ever after!",
+    "A perfect couple on a perfect day!",
+    "May your love grow stronger each day!",
+    "Cheers to a beautiful beginning!",
+    "Congratulations on finding your forever!",
+    "Wishing you endless love and happiness!"
+  ],
+  anniversary: [
+    "Happy Anniversary to a wonderful couple!",
+    "May your love continue to bloom!",
+    "Cheers to your journey together!",
+    "Another year of loving each other!",
+    "Wishing you many more happy years!",
+    "A toast to your lasting bond!",
+    "May this love story never end!",
+    "Happy Anniversary and best wishes!",
+    "You both are goals! Congrats!",
+    "Love grows stronger with time!"
+  ],
+  farewell: [
+    "Goodbye and good luck on your new journey!",
+    "Wishing you success in your next adventure!",
+    "You’ll be missed. Stay in touch!",
+    "Thanks for the memories. Farewell!",
+    "Hope your next chapter is amazing!",
+    "This isn’t goodbye, it’s see you later!",
+    "Good luck! You’ll do great things!",
+    "Farewell and best wishes always!",
+    "We’ll miss you more than you know!",
+    "Stay awesome, wherever you go!"
+  ]
+};
+
+module.exports = messages;
